8e580321da7f0537aed5e04fa22a6b0c83dd8615
Yannick Gregoire <hi@yannickgregoire.nl>
2019-01-23 21:37:38 +0100

    Hurray for spreading

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,5 @@ export const SET_TRICK = 'SET_TRICK';
 
 export const setTrick = ( trick ) => ({
     type: SET_TRICK,
-    name: trick.name,
-    background: trick.background,
-    color: trick.color,
+    ...trick
 });
